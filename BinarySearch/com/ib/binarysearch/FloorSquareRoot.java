4e2167b847b0e4042b28ad30ecd0b3f81a39a81d
ketki <ketki@ketki-Inspiron-3537>
2017-11-11 00:21:30 +0530

    Added comments.

--- BinarySearch/com/ib/binarysearch/FloorSquareRoot.java
+++ BinarySearch/com/ib/binarysearch/FloorSquareRoot.java
@@ -1,34 +1,49 @@
 package com.ib.binarysearch;
 
+/**
+ * Implement int sqrt(int x).
+ * 
+ * Compute and return the square root of x.
+ * 
+ * If x is not a perfect square, return floor(sqrt(x))
+ * 
+ * Example :
+ * 
+ * Input : 11 Output : 3
+ * 
+ * @author ketki
+ *
+ */
 public class FloorSquareRoot {
 
-	public int sqrt(int x){
-		if(x==0||x==1){
+	public int sqrt(int x) {
+		if (x == 0 || x == 1) {
 			return x;
 		}
-		long start=0, end=x/2, mid=0,ans=0;
-		//do binary search 
-		while(start<=end){
-			mid=(start+end)/2;
-			//if mid is square root then return mid
-			if(mid*mid==x){
-				return (int)mid;
+		long start = 0, end = x / 2, mid = 0, ans = 0;
+		// do binary search
+		while (start <= end) {
+			mid = (start + end) / 2;
+			// if mid is square root then return mid
+			if (mid * mid == x) {
+				return (int) mid;
 			}
-			//we update ans as we need floor . i.e. whenever we move closer to sqrt(x) 
-			 if(mid*mid<x){
-				start=mid+1;
-				ans=mid;
+			// we update ans as we need floor . i.e. whenever we move closer to
+			// sqrt(x)
+			if (mid * mid < x) {
+				start = mid + 1;
+				ans = mid;
 			}
-			//if mid*mid is greater than x
-			else{
-				end=mid-1;
+			// if mid*mid is greater than x
+			else {
+				end = mid - 1;
 			}
-		}//end of while loop
-		return (int)ans;
+		}// end of while loop
+		return (int) ans;
 	}
 
 	public static void main(String[] args) {
-		FloorSquareRoot fsr=new FloorSquareRoot();
+		FloorSquareRoot fsr = new FloorSquareRoot();
 		System.out.println(fsr.sqrt(17));
 
 	}
